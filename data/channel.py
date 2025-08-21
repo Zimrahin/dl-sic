13692a42fbfa30a9ec7202efc804826a968b8631
Zimrahin <diegobadillosj@gmail.com>
2025-08-21 16:43:39 +0200

    Add fading model for dataset generation

---
+++ data/channel.py
@@ -0,0 +1,72 @@
+# Should include
+# fading, sdd randomness in
+#     power_delay_profile (both in length and in its keys/values)
+#     Rician K-factor: random[0,10] -> 0 is Rayleigh. K -> infty -> AWGN channel
+#
+#
+#
+# frequency offsets,
+# awgn,
+# phase offsets,
+# time delays,
+# IQ imbalance
+
+from gnuradio import blocks, gr, channels
+import numpy as np
+
+
+def fading_model(
+    input_samples: np.ndarray,
+    sample_rate: float | int,
+    *,
+    rician_factor_k: float = 4.0,  # LOS / NLOS. If 0, equivalent to Rayleigh model
+    max_doppler_speed: float = 20,  # (m/s)
+    power_delay_profile: dict[float, float] = {
+        0.0: 1.0,
+        0.1: 0.99,
+        1.3: 0.97,
+    },  # keys: delays in samples; values: magnitudes
+    num_sinusoids: int = 16,
+    num_taps: int = 16  # FIR taps for fractional delays
+) -> np.ndarray:
+    """Frequency Selective Fading Model block from GNU Radio."""
+    # Note regarding max_doppler
+    # For a receiver moving at 5 m/s, the coherence time of the channel is much
+    # larger than the maximum packet size (~10 ms) for BLE or IEEE 802.15.4
+    c = 3e8  # Speed of light (m/s)
+    fc = 2.4e9  # Approximate carrier frequency at ISM band
+    max_doppler_freq = fc * (max_doppler_speed / c)
+    norm_max_doppler_freq = max_doppler_freq / sample_rate
+
+    rician_model = True  # Simply use Rician always. Rayleigh is just Rician with K = 0
+    seed: int = np.random.randint(-10000, 10000)
+
+    # For a flat fading model, use power_delay_profile = {0.0: 1.0}
+    power_delay_profile = sorted(power_delay_profile.items())
+    pdp_delays = tuple(delay for delay, _ in power_delay_profile)
+    pdp_magnitudes = tuple(magnitude for _, magnitude in power_delay_profile)
+
+    # Convert NumPy array to GNU Radio format
+    src = blocks.vector_source_c(input_samples.tolist(), False, 1, [])
+
+    # Instantiate the Frequency Selective Fading block
+    block = channels.selective_fading_model(
+        num_sinusoids,
+        norm_max_doppler_freq,
+        rician_model,
+        rician_factor_k,
+        seed,
+        pdp_delays,
+        pdp_magnitudes,
+        num_taps,
+    )
+
+    # Sink to collect the output
+    sink = blocks.vector_sink_c(1, 1024 * 4)
+
+    # Connect blocks and run
+    tb = gr.top_block()
+    tb.connect(src, block, sink)
+    tb.run()
+
+    return np.array(sink.data())
