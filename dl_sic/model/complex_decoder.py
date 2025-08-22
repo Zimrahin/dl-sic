460dc1f188e62741aea06ef81e848f64264114b5
Zimrahin <diegobadillosj@gmail.com>
2025-08-22 11:37:35 +0200

    Add dtype selection option.
    However, getting warningswhen using torch.complex32:
    UserWarning: ComplexHalf support is experimental and many operators don't support it yet.
    (Triggered internally at /pytorch/aten/src/ATen/EmptyTensor.cpp:55.)
    
    Also, Complex Layer Normalisation from complextorch doesn't support torch.complex32

--- dl_sic/model/complex_decoder.py
+++ dl_sic/model/complex_decoder.py
@@ -14,6 +14,7 @@ class ComplexDecoder(nn.Module):
         out_channels: int = 1,
         *,
         kernel_size: int = 3,  # J in paper, changed from 2 to 3 (even -> odd)
+        dtype: type = torch.complex64,
     ) -> None:
         super().__init__()
         padding = (kernel_size - 1) // 2
@@ -23,7 +24,7 @@ class ComplexDecoder(nn.Module):
             out_channels=out_channels,
             kernel_size=kernel_size,
             padding=padding,
-            dtype=torch.complex64,
+            dtype=dtype,
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -40,11 +41,12 @@ def test_model():
     in_channels = 128
     batch_size = 4
     signal_length = 2048  # T
-    model = ComplexDecoder(in_channels)
+    dtype = torch.complex64
+    model = ComplexDecoder(in_channels, dtype=dtype)
 
     print(f"Total Parameters: {sum(p.numel() for p in model.parameters()):,}")
 
-    input = torch.rand((batch_size, in_channels, signal_length), dtype=torch.complex64)
+    input = torch.rand((batch_size, in_channels, signal_length), dtype=dtype)
     output = model(input)  # Forward pass
 
     print("Input shape:", input.shape)
