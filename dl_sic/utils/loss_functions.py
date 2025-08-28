eb8e8e311f94667b1d4c05cd565dabd5222f72f2
Zimrahin <diegobadillosj@gmail.com>
2025-08-28 14:48:27 +0200

    Add slightly optimised comples MSE

--- dl_sic/utils/loss_functions.py
+++ dl_sic/utils/loss_functions.py
@@ -3,7 +3,8 @@ import torch
 
 def mse_loss_complex(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     """Complex-valued MSE loss function"""
-    return torch.mean(torch.abs(pred - target) ** 2)
+    difference = pred - target
+    return torch.mean((difference * difference.conj()).real)
 
 
 # Later define SI-SNR loss (Guo et al., 2024.)
