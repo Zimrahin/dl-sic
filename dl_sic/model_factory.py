32ff1dc7d3db4863065a6d03ca6ce7a6bd96991c
Zimrahin <diegobadillosj@gmail.com>
2025-11-13 18:46:35 +0100

    Add TCNConformerNet to model factory

--- dl_sic/model_factory.py
+++ dl_sic/model_factory.py
@@ -2,6 +2,7 @@ import torch
 
 from model.complex_tdcr_net import ComplexTDCRNet
 from model.real_tdcr_net import RealTDCRNet
+from model.tcn_conformer_net import TCNConformerNet
 
 
 class ModelFactory:
@@ -10,15 +11,23 @@ class ModelFactory:
         model_type: str, model_params: dict, dtype: torch.dtype, device: torch.device
     ):
         """Instantiate model"""
+        models: dict = {
+            "complextdcr": ComplexTDCRNet,
+            "tdcr": RealTDCRNet,
+            "tcnconformer": TCNConformerNet,
+        }
 
-        if model_type == "complextdcr":
-            model = ComplexTDCRNet(**model_params, dtype=dtype)
-        elif model_type == "tdcr":
-            if dtype.is_complex:
-                raise ValueError(f"Real TCDRNet cannot use complex dtype {dtype}")
-            model = RealTDCRNet(**model_params, dtype=dtype)
-        else:
+        if model_type not in models:
             raise ValueError(f"Unknown model type: {model_type}")
+
+        model_class = models[model_type]
+        if model_class not in [ComplexTDCRNet] and dtype.is_complex:
+            raise ValueError(f"Real models cannot use complex dtype {dtype}")
+
+        if model_class in [TCNConformerNet]:
+            model = model_class(**model_params)
+        else:
+            model = model_class(**model_params, dtype=dtype)
 
         print(f"Using model: {model.__class__.__name__}")
         return model.to(device)
