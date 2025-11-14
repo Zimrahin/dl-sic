8b986a7c65b755230be36db503c5bb3635a0dcbe
Zimrahin <diegobadillosj@gmail.com>
2025-11-14 11:28:01 +0100

    Add `.sh` for training TCNConformerNet on Slurm

---
+++ dl_sic/train_tcnconformer.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH --job-name=train_tcnc    # Job name equals shell file name
+#SBATCH --cpus-per-gpu=1         # Dataloader num_workers + 1 (>1 if multi-threaded tasks)
+#SBATCH --partition=gpu          # Name of the partition
+#SBATCH --gres=gpu:1             # Number and type of GPU cards and type allocated
+#SBATCH --mem=16G                # Total memory allocated
+#SBATCH --time=10:00:00          # Total run time limit (HH:MM:SS)
+#SBATCH --output=%x_%j.out       # Output file name
+#SBATCH --exclude=gpu009,gpu01[2-3],gpu01[5-7]  # Exclude team nodes
+
+echo "### Running $SLURM_JOB_NAME ###"
+
+set +x  # Turn off verbose mode
+cd ${SLURM_SUBMIT_DIR}
+
+module purge
+
+# Set Conda environment
+source /home/$USER/.bashrc
+# PyTorch environment should be created previously
+source activate pt_env
+
+python -u train.py \
+    --model_type "tcnconformer" \
+    --dtype "float32" \
+    --batch_size 1 \
+    --epochs 100 \
+    --learning_rate 1e-3 \
+    --weight_decay 0 \
+    --val_split 0.2 \
+    --target 1 \
+    --num_workers 0 \
+    --model_param_M 128 \
+    --model_param_N 64 \
+    --model_param_U 128 \
+    --model_param_V 3 \
+    --model_param_conformer_num_heads 4 \
+    --model_param_conformer_ffn_times_input 2 \
+    --model_param_conformer_num_layers 2 \
+    --model_param_conformer_conv_kernel_size 15 \
+    --checkpoints_dir "./checkpoints" \
+    --dataset_path "./data/simulated_dataset.pt" \
+    --runtime_gen
